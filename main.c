5548fabc95ba7c488ed72740f73f7272c3204db6
KuroOnee <Dim22Rus@gmail.com>
2016-03-24 12:49:27 +0600

    Realized main algorythm

--- main.c
+++ main.c
@@ -1,20 +1,50 @@
 #include <stdio.h>
 #include <locale.h>
-
-int sqrt(int a, int b, int c, int* x, int* y)
+#include <math.h>
+float x1,x2;
+int func(int a, int b, int c)
 {
-    
+    float d;
+    d = (b * b) - (4 * a * c);
+    printf ("%f",d);
+    if (d > 0)
+    {
+    x1 = (-b + sqrt(d))/ (2 * a);
+    x2 = (-b - sqrt(d))/ (2 * a);
+    return 1;
+    }
+    if (d == 0)
+    {
+    x1 = -b / (2 * a);
+    return 2;
+    }
+    return 0;
 }
+
 int main()
 {
-    int a,b,c,x1,x2;
+    float a,b,c;
+    int d;
     setlocale(LC_ALL, "Rus");
     printf("Введите a: ");
-    scanf("%d",&a);   
+    scanf("%f",&a);   
     printf("Введите b: ");
-    scanf("%d",&b);   
+    scanf("%f",&b);   
     printf("Введите c: ");
-    scanf("%d",&c);
-    sqrt(a,b,c,x1,x2);
+    scanf("%f",&c);
+    d = func(a,b,c);
+    printf ("%d",d);
+    if (d == 2)
+	{
+	printf("У уравнения 1 корень: %f",x1);
+	return 0;
+	}
+    if (d == 1)
+	{
+	printf("У уравнения 2 корня: %f %f",x1 ,x2);
+	return 0;
+	}
+    if (d == 0)
+	printf ("У уравнения нет корней. ");
     return 0;
 }
