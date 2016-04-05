826f18fba20e3292fd662e0101ba6eec7ed17d0c
KuroOnee <Dim22Rus@gmail.com>
2016-04-05 11:10:57 +0600

    Added const

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <locale.h>
 #include <math.h>
+#define nroots 0
+#define 1root 1
+#define 2roots 2
+#define error -1
 float x1, x2;
 int sqrtsolve(float a, float b, float c)
 {
@@ -10,19 +14,19 @@ int sqrtsolve(float a, float b, float c)
     {
     x1 = (-b + sqrt(d)) / (2 * a);
     x2 = (-b - sqrt(d)) / (2 * a);
-    return 2;
+    return 2roots;
     }
     if (d == 0)
     {
     x1 = -b / (2 * a);
-    return 1;
+    return 1root;
     }
-    return 0;
+    return nroots;
 }
 
 int main()
 {
-    float a,b,c;
+    float a, b, c;
     short k;
     setlocale(LC_ALL, "Rus");
     printf("Введите a: ");
@@ -31,18 +35,23 @@ int main()
     scanf("%f", &b);   
     printf("Введите c: ");
     scanf("%f", &c);
-    k = sqersolve(a, b, c);
-    if (k == 1)
+    k = sqrtsolve(a, b, c);
+    if (k == 1root)
 	{
 	printf("У уравнения 1 корень: %.2f", x1);
 	return 0;
 	}
-    if (k == 2)
+    if (k == 2roots)
 	{
 	printf("У уравнения 2 корня: %.2f %.2f", x1, x2);
 	return 0;
 	}
-    if (k == 0)
+    if (k == nroots)
+	{
 	printf ("У уравнения нет корней. ");
-    return 0;
+	return 0;
+	}
+    if (k == error)
+	printf ("Что-то пошло не так!");
+    return -1;
 }
