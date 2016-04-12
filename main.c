db68924df634427715db5de7ec137586b29e1a6a
KuroOnee <Dim22Rus@gmail.com>
2016-04-12 11:21:41 +0600

    Added some fix in 'sqrtsolve'

--- main.c
+++ main.c
@@ -8,23 +8,21 @@ int sqrtsolve(float a, float b, float c, float* f, float* e)
 {
     float d, x1, x2;
     if ((a == 0) && (b == 0) && (c == 0))
-	{printf("Это разве уравнение?");
 	 return -1;
-	}
     d = (b * b) - (4 * a * c);
     if (d > 0)
-    {
-    x1 = (-b + sqrt(d)) / (2 * a);
-    x2 = (-b - sqrt(d)) / (2 * a);
-    *f = x1;
-    *e = x2;
-    return Tworoots;
-    }
+	{
+	x1 = (-b + sqrt(d)) / (2 * a);
+	x2 = (-b - sqrt(d)) / (2 * a);
+	*f = x1;
+	*e = x2;
+	return Tworoots;
+	}
     if (d == 0)
-    {
-    x1 = -b / (2 * a);
-    f = &x1;
-    return Oneroot;
+	{
+	x1 = -b / (2 * a);
+	f = &x1;
+	return Oneroot;
     }
     return Noroots;
 }
